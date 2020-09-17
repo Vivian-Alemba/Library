3a9411b43cb700f2d4aff5b1d1cb61273514dbe6
vivian-alemba <alembavivianauma@gmail.com>
2020-09-17 18:16:14 +0300

    updated code

--- script.js
+++ script.js
@@ -1,61 +1,80 @@
-const title=document.getElementById("#Title")
-const author=document.getElementById("#author")
-const page=document.getElementById("#page")
-const read=document.getElementById("#check")
-const button=document.getElementById("#add")
-
-
+//declaring my input variables
+const titleInput=document.querySelector('#Title')
+const authorInput=document.querySelector('#author')
+const pagesInput=document.querySelector('#page')
+const readInput=document.querySelector('#check')
+const addButton=document.querySelector('#add')
+const bookForm=document.querySelector('#book-form')
+const bookList=document.querySelector('#list-books')
 
 
 let myLibrary=[]
 
+
 function Book(title,author,pages,read){
-    //creating a constructor for the book object
-    this.title=title
-    this.author=author
-    this.pages=pages
-    this.read=read
-    
-
-
-}
-Book.prototype.addBook=function(){
-    console.log(this.title)
-}
-
-
-
-
-function addBookToLibrary(title){
-    //adding my book to my library
-    this.title=title
-    this.author="Jacob Aliet"
-    this.pages=100
-    this.read="Not yet red"
-
-    for (let key in Book){
-        myLibrary.push(Book[key])
-        console.log(myLibrary.push(Book[key]))
-    }
-    
-    
-    
-
-   
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+    this.showBook=function(){
+        return this.title
+    };
 
 }
-addBookToLibrary.prototype.addBook=function(){
-    for (let key in Book){
-        myLibrary.push(Book[key])
-        console.log(myLibrary.push(Book[key]))
-    }
-    
-    console.log("js")
-}
-addBookToLibrary.prototype=Object.create(Book.prototype)
 
-const book=new addBookToLibrary("js")
-book.addBook()
+// let book1=new Book("My life story","Vivian Alemba",2,"read")
+// let book2=new Book("My life story","Vivian Alemba",2,"read")
+// let book3=new Book("My life story","Vivian Alemba",2,"read")
+
+// console.log(book1)
+
+
+function addBookToLibrary(){
+     if(titleInput.value== ""  || authorInput.value== "" || pagesInput.value== "" ||
+    readInput.value== ""){
+        alert("Enter inputs for the fields")
+
+   }else{
+       const bookListRow=document.createElement("tr")
+
+       const newTitle=document.createElement("td")
+       newTitle.innerHTML=titleInput.value
+       bookListRow.appendChild(newTitle)
+
+       const newAuthor=document.createElement("td")
+       newAuthor.innerHTML=authorInput.value;
+       bookListRow.appendChild(newAuthor)
+
+       const newPage=document.createElement("td")
+       newPage.innerHTML=pagesInput.value;
+       bookListRow.appendChild(newPage)
+
+    
+
+    //    const newRead=document.createElement("td")
+    //    newRead.innerHTML=readInput.value;
+    //    bookListRow.appendChild(newRead)
+
+       bookList.appendChild(bookListRow)
+
+       for(let i; i<myLibrary.length; i++){
+          return myLibrary[i].push(bookList)
+           
+       }
+       
+}
+}
+addButton.addEventListener('click',addBookToLibrary)
+
+
+
+
+
+
+
+
+
+
 
 
 
